adadf1ce675c174dc7c5b98ddb5bcf507e0f0313
andre-gregor-herrmann_wln <andre.gregor-herrmann@payone.com>
2026-02-18 11:51:59 +0100

    TPI-4499: Fix paths

--- src/Core/ViewConfig.php
+++ src/Core/ViewConfig.php
@@ -66,6 +66,6 @@ class ViewConfig extends ViewConfig_parent
             }
         }
 
-        return $this->getModuleUrl('PayonePcpPrototype', 'out/img/payone_logo.png');
+        return $this->getModuleUrl('PayonePcpPrototype', 'assets/img/payone_logo.png');
     }
 }
