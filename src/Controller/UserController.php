ebdad68b6a0245dd2e905c1aca1d9648df47abe4
andre-gregor-herrmann_wln <andre.gregor-herrmann@payone.com>
2026-02-18 14:37:39 +0100

    TPI-4499: Getting module config

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,20 +5,34 @@ declare(strict_types=1);
 namespace Payone\PcpPrototype\Controller;
 
 use OxidEsales\Eshop\Core\Registry;
+use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Bridge\ModuleConfigurationDaoBridgeInterface;
 
 class UserController extends UserController_parent
 {
     public function getLoginOption(): int
     {
+        $logger = Registry::getLogger();
         if ($this->pcpUseFixedNoregestration()) {
+            $logger->error('pcpUseFixedNoregestration is active, return fixed value for getLoginOption()');
             return 1;
         }
 
-        return parent::getLoginOption();
+        $logger->error('pcpUseFixedNoregestration is not active, return parent value for getLoginOption()');
+        return (int) parent::getLoginOption();
     }
 
     public function pcpUseFixedNoregestration(): bool
     {
-        return (bool) Registry::getConfig()->getConfigParam('pcpUseFixedNoregestration');
+        $container = ContainerFactory::getInstance()->getContainer();
+        $logger = Registry::getLogger();
+        $logger->error('Check if pcpUseFixedNoregestration is active');
+        $moduleConfigService = $container->get(ModuleConfigurationDaoBridgeInterface::class);
+        $moduleConfig = $moduleConfigService->get('PayonePcpPrototype');
+        if (!$moduleConfig->hasModuleSetting('pcpUseFixedNoregestration')) {
+            $logger->error('Module setting pcpUseFixedNoregestration not found, return false');
+            return false;
+        }
+        return (bool) $moduleConfig->getModuleSetting('pcpUseFixedNoregestration')->getValue();
     }
 }
